e245bbf36421cb4dc0ac8ef83dfddce66ea2f8ea
soccer <s6303051633093@kmutnb.ac.th>
2023-03-03 00:50:54 +0700

    3/03/23

---
+++ as-06/http/target/debug/deps/http-7a7af157a4517500.d
@@ -0,0 +1,7 @@
+c:\Users\Asus\Downloads\lab07-2023-02-24\http\target\debug\deps\http-7a7af157a4517500.rmeta: src\lib.rs src\httprequest.rs src\httpresponse.rs
+
+c:\Users\Asus\Downloads\lab07-2023-02-24\http\target\debug\deps\http-7a7af157a4517500.d: src\lib.rs src\httprequest.rs src\httpresponse.rs
+
+src\lib.rs:
+src\httprequest.rs:
+src\httpresponse.rs:
---
+++ as-06/http/target/debug/deps/http-9a5156ff0855bcdb.d
@@ -0,0 +1,7 @@
+c:\Users\Asus\Downloads\lab07-2023-02-24\http\target\debug\deps\http-9a5156ff0855bcdb.rmeta: src\lib.rs src\httprequest.rs src\httpresponse.rs
+
+c:\Users\Asus\Downloads\lab07-2023-02-24\http\target\debug\deps\http-9a5156ff0855bcdb.d: src\lib.rs src\httprequest.rs src\httpresponse.rs
+
+src\lib.rs:
+src\httprequest.rs:
+src\httpresponse.rs:
